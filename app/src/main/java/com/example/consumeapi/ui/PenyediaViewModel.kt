836598fca767675f84cb779e7a8ad0a580d4ad61
mhmdfakhrim <muhfakhri4@gmail.com>
2023-12-15 15:59:50 +0700

    activity

--- app/src/main/java/com/example/consumeapi/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/consumeapi/ui/PenyediaViewModel.kt
@@ -1,2 +1,20 @@
 package com.example.consumeapi.ui
 
+import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.consumeapi.KontakAplication
+import com.example.consumeapi.ui.home.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiMars().container.kontakRepository)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiMars(): KontakAplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KontakAplication)
--- app/src/main/java/com/example/consumeapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumeapi/ui/home/screen/HomeScreen.kt
@@ -1,2 +1,117 @@
 package com.example.consumeapi.ui.home.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.consumeapi.R
+import com.example.consumeapi.model.Kontak
+import com.example.consumeapi.ui.home.viewmodel.KontakUIState
+
+@Composable
+fun HomeScreen(
+    kontakUIState: KontakUIState,retryAction: () -> Unit, modifier: Modifier = Modifier
+){
+    when (kontakUIState) {
+        is KontakUIState.Loading -> OnLoading
+    }
+}
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error),
+            contentDescription = ""
+        )
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+@Composable
+fun KontakLayout(
+    kontak: List<Kontak>,
+    modifier: Modifier = Modifier
+
+){
+    LazyColumn(
+        modifier = Modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        items(kontak){
+                kontak ->
+            KontakCard(
+                kontak = kontak,
+                modifier = Modifier.fillMaxWidth()
+                                )
+        }
+    }
+}
+
+@Composable
+fun KontakCard(
+    kontak: Kontak,
+    modifier: Modifier = Modifier
+){
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ){
+        Column(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_Large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        ){
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ){
+                Text(
+                    text = kontak.nama,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Spacer(Modifier.weight(1f))
+                Icon(imageVector = Icons.Default.Phone,
+                    contentDescription = null,
+                )
+                Text(
+                    text = kontak.alamat,
+                    style = MaterialTheme.typography.titleMedium
+
+                )
+                Spacer(Modifier.weight(1f))
+                Text(
+                    text = kontak.nohp,
+                    style = MaterialTheme.typography.titleMedium
+
+                )
+            }
+        }
+    }
+}

bc72bc71f15cffddc5a917ddecf5ab002d65c3bc
mhmdfakhrim <muhfakhri4@gmail.com>
2023-12-15 15:22:25 +0700

    activity

--- app/src/main/java/com/example/consumeapi/KontakAplikation.kt
+++ app/src/main/java/com/example/consumeapi/KontakAplikation.kt
@@ -1,2 +1,13 @@
 package com.example.consumeapi
 
+import android.app.Application
+import com.example.consumeapi.repository.AppContainer
+import com.example.consumeapi.repository.KontakContainer
+
+class KontakAplication : Application() {
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container = KontakContainer()
+    }
+}

d99b7b307f00060c76f1aac1650768698d568de3
mhmdfakhrim <muhfakhri4@gmail.com>
2023-12-28 20:03:07 +0700

    activity

--- app/src/main/java/com/example/consumeapi/repository/KontakRepository.kt
+++ app/src/main/java/com/example/consumeapi/repository/KontakRepository.kt
@@ -2,13 +2,48 @@ package com.example.consumeapi.repository
 
 import com.example.consumeapi.model.Kontak
 import com.example.consumeapi.network.KontakService
+import java.io.IOException
 
 interface KontakRepository{
     suspend fun getKontak(): List<Kontak>
+
+    suspend fun insertKontak(kontak: Kontak)
+
+    suspend fun updateKontak(id: Int, kontak: Kontak)
+
+    suspend fun deleteKontak(id: Int)
+
+    suspend fun getKontakById(id: Int): Kontak
 }
 
 class NetworkKontakRepository(
     private val kontakApiService: KontakService
 ) : KontakRepository {
     override suspend fun getKontak(): List<Kontak> = kontakApiService.getKontak()
+
+    // insert kontak
+    override suspend fun insertKontak(kontak: Kontak) {
+        kontakApiService.insertKontak(kontak)
+    }
+
+    // update kontak
+    override suspend fun updateKontak(id: Int, kontak: Kontak) {
+        kontakApiService.updateKontak(id, kontak)
+    }
+
+    override suspend fun deleteKontak(id: Int) {
+        try {
+            val response = kontakApiService.deleteKontak(id)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete kontak. HTTP status code: ${response.code()}")
+            } else {
+                response.message()
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+    override suspend fun getKontakById(id: Int): Kontak {
+        return kontakApiService.getKontakById(id)
+    }
 }

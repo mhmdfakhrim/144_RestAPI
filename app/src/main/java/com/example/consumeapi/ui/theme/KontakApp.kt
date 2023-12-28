17b3748d19e55f68465a1a528f1ec5e09be2c074
mhmdfakhrim <muhfakhri4@gmail.com>
2023-12-28 20:23:17 +0700

    activity

---
+++ app/src/main/java/com/example/consumeapi/ui/theme/KontakApp.kt
@@ -0,0 +1,34 @@
+package com.example.consumeapi.ui.theme
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.consumeapi.navigation.PengelolaHalaman
+import com.example.consumeapi.ui.home.HomeViewModel
+import com.example.consumeapi.ui.home.viewmodel.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun KontakApp(
+    homeViewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            PengelolaHalaman()
+        }
+    }
+}

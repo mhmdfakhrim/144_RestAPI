da3bbcfd3e714087cbe83036d40a68dd2365129d
mhmdfakhrim <muhfakhri4@gmail.com>
2023-12-28 18:52:40 +0700

    activity

---
+++ app/src/main/java/com/example/consumeapi/ui/home/viewmodel/InsertViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.consumeapi.ui.home.viewmodel
+
+import com.example.consumeapi.repository.KontakRepository
+
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent) {
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak() {
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}

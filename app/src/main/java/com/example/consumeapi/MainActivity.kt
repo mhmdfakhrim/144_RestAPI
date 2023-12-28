eb69a9afbd27bacfc2add9bc11a514e408572fdc
mhmdfakhrim <muhfakhri4@gmail.com>
2023-12-28 20:42:45 +0700

    activity

--- app/src/main/java/com/example/consumeapi/MainActivity.kt
+++ app/src/main/java/com/example/consumeapi/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.consumeapi.ui.theme.ConsumeapiTheme
+import com.example.consumeapi.ui.theme.KontakApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
